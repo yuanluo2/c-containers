6525b9d3ffaaa6da7c442cae7ca32ab19798c87b
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-06-24 16:52:45 +0800

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,38 @@
+import shutil
+
+def do_file_replace(filePath, replaceMap):
+    with open(filePath, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+
+    for pattern, target in replaceMap.items():
+        file_content = file_content.replace(pattern, target)
+
+    with open(filePath, 'w', encoding='utf-8') as file:
+        file.write(file_content)
+
+
+def create_array(targetFilePath, elementType, arrayTypeName):
+    replaceMap = {
+        '@ElementType': elementType,
+        '@ArrayTypeName': arrayTypeName
+    }
+
+    templateFile = './template_array.txt'
+    shutil.copyfile(templateFile, targetFilePath)
+    do_file_replace(targetFilePath, replaceMap)
+
+
+def create_doubly_linked_list(targetFilePath, elementType, listNodeTypeName, listTypeName):
+    replaceMap = {
+        '@ElementType': elementType,
+        '@listNodeTypeName': listNodeTypeName,
+        '@listTypeName': listTypeName
+    }
+
+    templateFile = './template_doubly_linked_list.txt'
+    shutil.copyfile(templateFile, targetFilePath)
+    do_file_replace(targetFilePath, replaceMap)
+
+
+if __name__ == '__main__':
+    create_array('array_int64.c', 'int64_t', 'ArrayInt64')
