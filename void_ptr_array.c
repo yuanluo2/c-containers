30f3dd2da4945f5d2938e201be98d3c91d037c03
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-06-25 11:05:51 +0800

    Add files via upload

---
+++ void_ptr_array.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+typedef struct GenericArray {
+    void* data;
+    size_t capacity;
+    size_t length;
+    size_t elemSize;
+} GenericArray;
+
+#define GenericArray_At(arrPtr, index) \
+    ((char*)((arrPtr)->data) + (index) * (arrPtr)->elemSize)
+
+#define GenericArray_Capacity(arrPtr)   ((arrPtr)->capacity)
+#define GenericArray_Length(arrPtr)     ((arrPtr)->length)
+#define GenericArray_Data(arrPtr)       ((arrPtr)->data)
+#define GenericArray_Clear(arrPtr)      GenericArray_Length(arrPtr) = 0;
+#define GenericArray_IsEmpty(arrPtr)    (GenericArray_Length(arrPtr) == 0)
+#define GenericArray_Front(arrPtr)      GenericArray_At(arrPtr, 0)
+#define GenericArray_Back(arrPtr)       GenericArray_At(arrPtr, GenericArray_Length(arrPtr) - 1)
+
+#define GenericArray_ForEach(arrPtr, cursor) \
+    for (cursor = 0; cursor < GenericArray_Length(arrPtr); ++cursor)
+
+#define GenericArray_ForEachReverse(arrPtr, cursor) \
+    for (cursor = GenericArray_Length(arrPtr) - 1; cursor >= 0; --cursor)
+
+GenericArray* GenericArray_CreateNew(size_t capacity, size_t elemSize) {
+    if (elemSize == 0) {
+        return NULL;
+    }
+
+    GenericArray* arr = (GenericArray*)malloc(sizeof(GenericArray));
+    if (arr == NULL) {
+        return NULL;
+    }
+
+    arr->elemSize = elemSize;
+    arr->length = 0;
+    arr->capacity = (capacity == 0 ? 15 : capacity);
+
+    if ((arr->data = malloc(arr->capacity * elemSize)) == NULL) {
+        free(arr);
+        return NULL;
+    }
+
+    return arr;
+}
+
+void GenericArray_Destroy(GenericArray* arr) {
+    free(arr->data);
+    free(arr);
+}
+
+int GenericArray_ExpandCapacity(GenericArray* arr, size_t newCapacity) {
+    void* temp = realloc(arr->data, newCapacity * arr->elemSize);
+    if (temp == NULL) {
+        return 0;
+    }
+
+    arr->data = temp;
+    arr->capacity = newCapacity;
+    return 1;
+}
+
+void* GenericArray_PushBack(GenericArray* arr) {
+    if (arr->length == arr->capacity) {
+        if (!GenericArray_ExpandCapacity(arr, 2 * arr->capacity)) {
+            return NULL;
+        }
+    }
+
+    arr->length += 1;
+    return (void*)GenericArray_At(arr, arr->length - 1);
+}
+
+void GenericArray_Remove(GenericArray* arr, size_t index) {
+    if (index >= GenericArray_Length(arr)) {
+        return;
+    }
+
+    size_t i;
+    for (i = index; i < GenericArray_Length(arr); ++i) {
+        memcpy(GenericArray_At(arr, i), GenericArray_At(arr, i + 1), arr->elemSize);
+    }
+
+    arr->length -= 1;
+}
+
+void GenericArray_PopFront(GenericArray* arr) {
+    GenericArray_Remove(arr, 0);
+}
+
+void GenericArray_PopBack(GenericArray* arr) {
+    GenericArray_Remove(arr, GenericArray_Length(arr) - 1);
+}
+
+int main() {
+    GenericArray* arr = GenericArray_CreateNew(5, sizeof(long long));
+
+    long long i;
+    long long* data;
+    for (i = 0; i < 10; ++i) {
+        data = GenericArray_PushBack(arr);
+        *data = i * i;        
+    }
+
+    GenericArray_PopFront(arr);
+    GenericArray_Remove(arr, 3);
+
+    GenericArray_ForEach(arr, i) {
+        printf("%lld\n", *(long long*)GenericArray_At(arr, i));
+    }
+
+    GenericArray_Destroy(arr);
+    return 0;
+}
---
+++ void_ptr_doubly_linked_list.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+typedef struct GenericDoublyListNode {
+    struct GenericDoublyListNode* prev;
+    struct GenericDoublyListNode* next;
+} GenericDoublyListNode;
+
+typedef struct GenericDoublyList {
+    GenericDoublyListNode* head;
+    GenericDoublyListNode* tail;
+    size_t length;
+    size_t elemSize;
+} GenericDoublyList;
+
+#define GenericDoublyList_NodePrev(nodePtr)   ((nodePtr)->prev)
+#define GenericDoublyList_NodeNext(nodePtr)   ((nodePtr)->next)
+#define GenericDoublyList_NodeData(nodePtr)   ((nodePtr) + 1)
+
+#define GenericDoublyList_Length(listPtr)     ((listPtr)->length)
+#define GenericDoublyList_Front(listPtr)      ((listPtr)->head)
+#define GenericDoublyList_Back(listPtr)       ((listPtr)->tail)
+#define GenericDoublyList_IsEmpty(listPtr)    (GenericDoublyList_Length(listPtr) == 0)
+
+#define GenericDoublyList_ForEach(listPtr, nodePtr) \
+    for ((nodePtr) = (listPtr)->head; (nodePtr) != NULL; (nodePtr) = (nodePtr)->next)
+
+#define GenericDoublyList_ForEachReverse(listPtr, nodePtr) \
+    for ((nodePtr) = (listPtr)->tail; (nodePtr) != NULL; (nodePtr) = (nodePtr)->prev)
+
+GenericDoublyList* GenericDoublyList_CreateNew(size_t elemSize) {
+    if (elemSize == 0) {
+        return NULL;
+    }
+
+    GenericDoublyList* list = (GenericDoublyList*)malloc(sizeof(GenericDoublyList));
+    if (list == NULL) {
+        return NULL;
+    }
+
+    list->head = list->tail = NULL;
+    list->length = 0;
+    list->elemSize = elemSize;
+    return list;
+}
+
+void GenericDoublyList_Clear(GenericDoublyList* list) {
+    GenericDoublyListNode* node = list->head;
+
+    while (list->head != NULL) {
+        node = node->next;
+        free(list->head);
+        list->head = node;
+    }
+
+    list->length = 0;
+}
+
+void GenericDoublyList_Destroy(GenericDoublyList* list) {
+    GenericDoublyList_Clear(list);
+    free(list);
+}
+
+void* GenericDoublyList_PushBack(GenericDoublyList* list) {
+    GenericDoublyListNode* node = (GenericDoublyListNode*)malloc(sizeof(GenericDoublyListNode) + list->elemSize);
+    if (node == NULL) {
+        return NULL;
+    }
+
+    if (list->head == NULL) {
+        node->prev = node->next = NULL;
+        list->head = list->tail = node;
+    }
+    else {
+        node->prev = list->tail;
+        node->next = NULL;
+        list->tail->next = node;
+        list->tail = list->tail->next;
+    }
+
+    list->length += 1;
+    return (void*)(node + 1);
+}
+
+void* GenericDoublyList_PushFront(GenericDoublyList* list) {
+    GenericDoublyListNode* node = (GenericDoublyListNode*)malloc(sizeof(GenericDoublyListNode) + list->elemSize);
+    if (node == NULL) {
+        return NULL;
+    }
+
+    if (list->head == NULL) {
+        node->prev = node->next = NULL;
+        list->head = list->tail = node;
+    }
+    else {
+        node->prev = NULL;
+        node->next = list->head;
+        list->head = node;
+    }
+
+    list->length += 1;
+    return (void*)(node + 1);
+}
+
+GenericDoublyListNode* GenericDoublyList_RemoveNode(GenericDoublyList* list, GenericDoublyListNode* node, int isBackOrder) {
+    GenericDoublyListNode* retNode;
+
+    if (list->head == list->tail) {
+        list->head = list->tail = NULL;
+        retNode = NULL;
+    }
+    else {
+        if (node == list->head) {
+            list->head = node->next;
+            list->head->prev = NULL;
+            retNode = isBackOrder ? list->head : NULL;
+        }
+        else if (node == list->tail) {
+            list->tail = node->prev;
+            list->tail->next = NULL;
+            retNode = isBackOrder ? NULL : list->tail;
+        }
+        else {
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+            retNode = isBackOrder ? node->next : node->prev;
+        }
+    }
+
+    free(node);
+    list->length -= 1;
+    return retNode;
+}
+
+void GenericDoublyList_PopFront(GenericDoublyList* list) {
+    if (GenericDoublyList_IsEmpty(list)) {
+        return;
+    }
+
+    GenericDoublyList_RemoveNode(list, list->head, 1);
+}
+
+void GenericDoublyList_PopBack(GenericDoublyList* list) {
+    if (GenericDoublyList_IsEmpty(list)) {
+        return;
+    }
+
+    GenericDoublyList_RemoveNode(list, list->tail, 1);
+}
+
+int main() {
+    GenericDoublyList* list = GenericDoublyList_CreateNew(sizeof(int));
+
+    int i;
+    int* ptr;
+    for (i = 0; i < 10; ++i) {
+        ptr = GenericDoublyList_PushBack(list);
+        *ptr = i * i;
+    }
+
+    GenericDoublyListNode* node;
+    for (node = list->head; node != NULL;) {
+        if (*(int*)GenericDoublyList_NodeData(node) % 2 != 0) {
+            node = GenericDoublyList_RemoveNode(list, node, 1);
+        }
+        else {
+            node = node->next;
+        }
+    }
+
+    GenericDoublyList_ForEach(list, node) {
+        printf("%d\n", *(int*)GenericDoublyList_NodeData(node));
+    }
+
+    GenericDoublyList_Destroy(list);
+    return 0;
+}
